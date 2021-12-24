383b8e20df46c9164840a37ac0b4878bb8c6ad81
Seweryn Woźniak <sewerynwozniak111$gmail.com>
2021-12-24 09:39:57 +0100

    adding automatic slider

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ class Carousel{
         this.btnNavigationNext = document.querySelector('.carousel__btnNavigation--next');
         this.singles = document.querySelectorAll('.carousel__single');
         this.singlesWrapper = document.querySelector('.carousel__singleWrapper');
+        this.automaticCarouselInterval;
         this.events();
+        this.automaticCarousel()
     }
 
     events(){
@@ -12,9 +14,27 @@ class Carousel{
         this.btnNavigationNext.addEventListener('click', ()=>this.move('right'))
     }
 
+    automaticCarousel(){
+        this.automaticCarouselInterval = setInterval(() => {
+            const singleWrapperChildren = document.querySelector('.carousel__singleWrapper').children;     
+            const singleWidth = singleWrapperChildren[0].offsetWidth;
+
+            const lastSingle = singleWrapperChildren[singleWrapperChildren.length-1];
+            lastSingle.style.width=0;
+            this.singlesWrapper.prepend(lastSingle);
+            setTimeout(()=>{                 
+                lastSingle.style.width=`${singleWidth}px`;
+            },0)
+
+        }, 3000);
+    }
+
     move(direction){
         const singleWrapperChildren = document.querySelector('.carousel__singleWrapper').children;     
         const singleWidth = singleWrapperChildren[0].offsetWidth;
+
+        clearInterval(this.automaticCarouselInterval)
+
         
         if(direction=='left'){
             const firstSingle = singleWrapperChildren[0];
@@ -32,9 +52,7 @@ class Carousel{
             },0)
         }
 
-      
-        
-
+        this.automaticCarousel()
 
     }
 
