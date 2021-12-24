f48fc52a622defd2ecc825c22622e49b110e90ca
Seweryn Woźniak <sewerynwozniak111$gmail.com>
2021-12-24 09:26:02 +0100

    adding manual operating with transition

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 class Carousel{
     constructor(){
-        this.moved=0;
         this.btnNavigationPrev = document.querySelector('.carousel__btnNavigation--prev');
         this.btnNavigationNext = document.querySelector('.carousel__btnNavigation--next');
         this.singles = document.querySelectorAll('.carousel__single');
+        this.singlesWrapper = document.querySelector('.carousel__singleWrapper');
         this.events();
     }
 
@@ -13,10 +13,33 @@ class Carousel{
     }
 
     move(direction){
-        console.log('moving ' + direction)
-        this.moved = this.moved+100;
-        this.singles.forEach(s=>s.style.transform=`translateX(-${this.moved}%)`)
+        const singleWrapperChildren = document.querySelector('.carousel__singleWrapper').children;     
+        const singleWidth = singleWrapperChildren[0].offsetWidth;
+        
+        if(direction=='left'){
+            const firstSingle = singleWrapperChildren[0];
+            firstSingle.style.width=0;
+            setTimeout(()=>{      
+                this.singlesWrapper.append(firstSingle);
+                firstSingle.style.width=`${singleWidth}px`;
+            },300)
+        }else{
+            const lastSingle = singleWrapperChildren[singleWrapperChildren.length-1];
+            lastSingle.style.width=0;
+            this.singlesWrapper.prepend(lastSingle);
+            setTimeout(()=>{                 
+                lastSingle.style.width=`${singleWidth}px`;
+            },0)
+        }
+
+      
+        
+
+
     }
+
+        
+
 
 }
 
